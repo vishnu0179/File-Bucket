8387404123f4abe68288ae984ab5d5ae4411bb33
vishnu0179 <vjvj140@gmail.com>
2019-07-10 21:31:27 +0530

    Removed aws api keys

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-    AWS_ACCESS_KEY: 'AKIASWQE4JS7ELSRYGTW',
-    AWS_SECRET_ACCESS_KEY: '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',
+    AWS_ACCESS_KEY: '',
+    AWS_SECRET_ACCESS_KEY: '',
     REGION : 'us-east-2',
     Bucket: 'unknown-project-file-upload'
 

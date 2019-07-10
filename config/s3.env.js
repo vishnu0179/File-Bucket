16cc7cef5538547315d65f28d875417514634562
vishnu0179 <vjvj140@gmail.com>
2019-07-11 01:50:37 +0530

    enviroment variable or aws keys

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-    AWS_ACCESS_KEY: '',
-    AWS_SECRET_ACCESS_KEY: '',
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     REGION : 'us-east-2',
     Bucket: 'unknown-project-file-upload'
 
